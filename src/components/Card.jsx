d6deb28ce59403661d91017993a17f1096441406
dp-daly <dan.daly@education.gov.uk>
2024-06-07 14:35:22 +0100

    title overflow managed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,8 @@ return (
     <div id="img-placeholder" style={{backgroundImage: `url(https://www.artic.edu/iiif/2/${image}/full/843,/0/default.jpg)`}}>
     </div>
     <div id="card-footer">
-    <h3>{artist}</h3>
-    <h4><i>{title}</i></h4>
+    <h3 id="artist-title">{artist}</h3>
+    <h4 id="card-title"><i>{title}</i></h4>
     </div>
 </div>
 )
